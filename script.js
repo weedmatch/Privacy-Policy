e67eaf4f372ae576daca0e98bcda61053bc28680
WeedMatch <121552068+weedmatch@users.noreply.github.com>
2026-07-23 00:07:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -197,3 +197,65 @@ if (sliderImages.length > 0) {
     setInterval(changeSliderImage, 3000);
 
 }
+
+
+
+
+
+
+
+
+
+
+<script>
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+
+faqItems.forEach(item => {
+
+
+    const button = item.querySelector(".faq-question");
+
+
+    button.addEventListener("click", () => {
+
+
+        faqItems.forEach(other => {
+
+            if(other !== item){
+
+                other.classList.remove("active");
+
+                other.querySelector(".faq-answer").style.maxHeight = null;
+
+            }
+
+        });
+
+
+
+        item.classList.toggle("active");
+
+
+        const answer = item.querySelector(".faq-answer");
+
+
+        if(item.classList.contains("active")){
+
+            answer.style.maxHeight = answer.scrollHeight + "px";
+
+        } else {
+
+            answer.style.maxHeight = null;
+
+        }
+
+
+    });
+
+
+});
+
+
+</script>
